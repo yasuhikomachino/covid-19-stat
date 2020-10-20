5989e1e5e88491ab452cb144cc86757529f67425
Yasuhiko Machino <yasuhikomachino@gmail.com>
2020-10-20 23:34:16 +0900

    Add `rel="noopener noreferrer”`

--- src/src/components/Footer/Footer.tsx
+++ src/src/components/Footer/Footer.tsx
@@ -6,15 +6,36 @@ const Footer: React.FC = () => {
         <Container className="my-5 py-5 text-left border-top">
             <p className="mb-0">
                 <span className="text-muted">Author: </span>
-                <a href="https://github.com/yasuhikomachino" target="blank" className="text-info">https://github.com/yasuhikomachino</a>
+                <a
+                    href="https://github.com/yasuhikomachino"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-info"
+                >
+                    https://github.com/yasuhikomachino
+                </a>
             </p>
             <p className="mb-0">
                 <span className="text-muted">Repository: </span>
-                <a href="https://github.com/yasuhikomachino/covid-19-stat" target="blank" className="text-info">https://github.com/yasuhikomachino/covid-19-stat</a>
+                <a
+                    href="https://github.com/yasuhikomachino/covid-19-stat"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-info"
+                >
+                    https://github.com/yasuhikomachino/covid-19-stat
+                </a>
             </p>
             <p className="mb-0">
                 <span className="text-muted">Data Source: </span>
-                <a href="https://covid19api.com/" target="blank" className="text-info">https://covid19api.com/</a>
+                <a
+                    href="https://covid19api.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-info"
+                >
+                    https://covid19api.com/
+                </a>
             </p>
         </Container>
     );
